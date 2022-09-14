f73acd247dd68b569f90291f340dd75f1735c717
yaroubh <yaroubhussein@tamu.edu>
2022-09-14 00:37:35 -0500

    Finished Project

--- public_html/app2.js
+++ public_html/app2.js
@@ -3,7 +3,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 
-//Making a click even for the menu button
+//Making a click event for the menu button
 menuBtn.addEventListener('click', () => {
     //toggling the menu-open class when button is pressed
     menu.classList.toggle('menu-open');
